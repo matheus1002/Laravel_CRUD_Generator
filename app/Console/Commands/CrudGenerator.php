5e1999ab6e0480871e283eb84d1f7be14d5eac11
Matheus <matheus10carneiro@gmail.com>
2020-01-03 12:19:49 -0300

    First commit

---
+++ app/Console/Commands/CrudGenerator.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CrudGenerator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crud:generator {name : Class (singular) for example User}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create CRUD operations';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+
+        $this->controller($name);
+        $this->model($name);
+        $this->request($name);
+
+        File::append(base_path('routes/api.php'), 'Route::resource(\''.Str::plural(strtolower($name)). "', '{$name}Controller');");
+
+        Artisan::call('make:migration create_' . strtolower(Str::plural($name)) . '_table --create=' . strtolower(Str::plural($name)));
+    }
+
+    protected function getStub($type)
+    {
+        return file_get_contents(resource_path("stubs/$type.stub"));
+    }
+
+    protected function model($name)
+    {
+        $template = str_replace(
+            ['{{modelTemplate}}'],
+            [$name],
+            $this->getStub('Model') 
+        );
+
+        file_put_contents(app_path("/{$name}.php"), $template);
+    }
+
+    protected function request($name)
+    {
+        $template = str_replace(
+            ['{{modelTemplate}}'],
+            [$name],
+            $this->getStub('Request') 
+        );
+
+        if(!file_exists($path = app_path('/Http/Requests')))
+            mkdir($path, 0777, true);
+
+        file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $template);
+    }
+
+    protected function controller($name)
+    {
+        $template = str_replace(
+            [
+                '{{modelTemplate}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}'
+            ],
+            [
+                $name,
+                strtolower(Str::plural($name)),
+                strtolower($name)
+            ],
+            $this->getStub('Controller') 
+        );
+
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $template);
+    }
+}
